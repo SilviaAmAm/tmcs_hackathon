e81c0976c2834053049215d47e90c577d550e4ea
IsabelTMCS18 <gonzalvezpi@gmail.com>
2018-05-31 15:32:04 +0100

    Modified Task 6,tested

--- Trim_CM.py
+++ Trim_CM.py
@@ -10,20 +10,26 @@ where: n_samples is the number of data points in the data set and n_atoms is the
 
 The outputs should have the following dimensions:
 trimmed_CM - numpy array of floats of shape (n_samples, n_trimmed_features)
-
-where: n_trimmed_features is n_features*(n_features-1)/2
+n_atoms= sqrt(n_feat)
+where: n_trimmed_features is n_atoms*(n_atoms-1))/2 + n_atoms
 """
 
 import math
 import numpy as np
 
 def Trim_CM(CM):
+    """
+    takes as an input a symmetric matrix and returns  a matrix with reduced dimensions that include the upper triangular part of it.
+    :param CM: numpy array of floats of shape (n_samples, n_features)
+    :return: numpy array of floats of shape (n_samples, n_trimmed_features)
+    """
 
     n_samples, n_features = np.shape(CM)                    #   GET THE N_samples and n_features from the CM matrix
-    n_trim= n_features*((n_features-1)/2)                   #   Reduced dimensions
-    trimmed_CM = np.zeros((n_samples * n_trim))
+    num_sq = int(math.sqrt(n_features))
+    n_trim= int(num_sq*(num_sq-1)/2 + num_sq)               #   Reduced dimensions
 
-    num_sq=math.sqrt(n_features)
+    trimmed_CM = np.zeros((n_samples, n_trim))
+
     for i in range(n_samples):
         working_mat=np.reshape(CM[i],[num_sq,num_sq])       #   Re-shape each row in CM to a square matrix
 
@@ -32,11 +38,13 @@ def Trim_CM(CM):
             for w in range(n,num_sq):
                 data_list.append(working_mat[n,w])          #   Back to row dimensions for the upper-triangle
 
-        for j in range(len(data_list)):
-
-            trimmed_CM[i,j]=data_list[j]                    #   Trimmed CM composed
+        trimmed_CM[i, :] = data_list                        #   Trimmed CM composed
 
     return trimmed_CM
 
+if __name__ == "__main__":                   
+    C=np.array([[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9]])
+    print(C)
+    M=Trim_CM(C)
 
-
+    print(M)
