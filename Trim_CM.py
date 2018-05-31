da3bac9b8aaefabbd2fbf42cbcea51b857b92b2b
IsabelTMCS18 <gonzalvezpi@gmail.com>
2018-05-31 13:33:17 +0100

    Task 6 before lunch :)

--- Trim_CM.py
+++ Trim_CM.py
@@ -13,3 +13,30 @@ trimmed_CM - numpy array of floats of shape (n_samples, n_trimmed_features)
 
 where: n_trimmed_features is n_features*(n_features-1)/2
 """
+
+import math
+import numpy as np
+
+def Trim_CM(CM):
+
+    n_samples, n_features = np.shape(CM)                    #   GET THE N_samples and n_features from the CM matrix
+    n_trim= n_features*((n_features-1)/2)                   #   Reduced dimensions
+    trimmed_CM = np.zeros((n_samples * n_trim))
+
+    num_sq=math.sqrt(n_features)
+    for i in range(n_samples):
+        working_mat=np.reshape(CM[i],[num_sq,num_sq])       #   Re-shape each row in CM to a square matrix
+
+        data_list = []
+        for n in range(num_sq):
+            for w in range(n,num_sq):
+                data_list.append(working_mat[n,w])          #   Back to row dimensions for the upper-triangle
+
+        for j in range(len(data_list)):
+
+            trimmed_CM[i,j]=data_list[j]                    #   Trimmed CM composed
+
+    return trimmed_CM
+
+
+
