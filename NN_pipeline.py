8e91c0b4c7375d6079a32eb1f65b9184a51eb8e4
HarryWTMorgan <harry.morgan@chem.ox.ac.uk>
2018-06-01 14:46:15 +0100

    added a correlation plot for the fit of the training data to NN_pipeline

--- NN_pipeline.py
+++ NN_pipeline.py
@@ -9,6 +9,7 @@ from SD_plain_coulomb_matrix import plain_cm
 from Trim_CM import Trim_CM
 import sklearn.model_selection as modsel
 from sklearn.neural_network import MLPRegressor
+import matplotlib.pyplot as plt
 
 import Extract_data
 
@@ -40,7 +41,7 @@ print(y_train.shape)
 
 ## Create an object MLPRegressor
 
-estimator = MLPRegressor(solver='adam',max_iter=1000,alpha=0,learning_rate_init=0.0001)
+estimator = MLPRegressor(solver='adam',max_iter=5000,alpha=0,learning_rate_init=0.0001)
 
 
 ## Fit the regressor
@@ -49,4 +50,8 @@ estimator.fit(x_train, y_train)
 ## Score the regressor
 score = estimator.score(x_train, y_train)
 print(score)
+
+y_predicted = estimator.predict(x_train)
+
+plt.scatter(y_train,y_predicted)
 ## Make predictions for the test set
