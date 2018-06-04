b285efd6ba19c68b4b726f47db11b6bcaf09b804
Silvia Amabilino <walfits@gmail.com>
2018-06-04 13:54:36 +0100

    Merged from remote

--- Eigenspectrum.py
+++ Eigenspectrum.py
@@ -16,8 +16,8 @@ where: n_eigenvalues is the number of eigenvalues of the Coulomb matrix (i.e. - 
 import numpy as np
 
 def diagonalise(cm_array):
-    eigenspectrum_mat = np.zeros(shape=(len(cm_array[:,0]), 7))
-    for i in range(len(cm_array[:, 0])):
+    eigenspectrum_mat = np.zeros(shape=(cm_array.shape[0], 7))
+    for i in range(cm_array.shape[0]):
         to_diag = np.reshape(cm_array[i, :], (7, 7))
         energy = np.linalg.eigvals(to_diag)
         energy = np.sort(energy)[::-1]
